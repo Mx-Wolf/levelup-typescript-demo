1d47dc4325d9e87bc05a17017035db890cabce44
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-12-29 11:00:04 +0300

    minor formatting

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { createElement, render } from './utils';
-import {getTemplate as getHeaderTemplate} from './views/header/header';
-import {getTemplate as getLogbookTemplate} from './views/log-book/log-book';
+import { getTemplate as getHeaderTemplate } from './views/header/header';
+import { getTemplate as getLogbookTemplate } from './views/log-book/log-book';
 
 const enum LogbookAppMessages {
   CONTAINER_NOT_FOUND = 'LogbookAppMessages.CONTAINER_NOT_FOUND'
 }
 
-const main = (container: HTMLElement|null)=>{
-  if(container === null){
+const main = (container: HTMLElement | null) => {
+  if (container === null) {
     throw new Error(LogbookAppMessages.CONTAINER_NOT_FOUND);
   }
   render(container, createElement(getHeaderTemplate()));
